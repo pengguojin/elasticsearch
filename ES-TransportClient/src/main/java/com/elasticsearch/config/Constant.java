6937a7059c40f5086ea4ef9762d3af8efd26c1a0
jin <529038529@qq.com>
2018-11-26 09:42:48 +0800

    同步

---
+++ ES-TransportClient/src/main/java/com/elasticsearch/config/Constant.java
@@ -0,0 +1,6 @@
+package com.elasticsearch.config;
+
+public class Constant {
+    public final static String index = "user";
+    public final static String type = "teacher";
+}
--- ES5/src/main/java/com/elasticsearch/config/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.elasticsearch.config;
-
-public class Constant {
-    public final static String index = "user";
-    public final static String type = "teacher";
-}
